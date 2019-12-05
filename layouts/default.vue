519ab082ad9520bb6a659b6a325226797bfb0eeb
arthur33277 <xazy06@gmail.com>
2019-12-05 10:54:36 +0300

    mr push

---
+++ layouts/default.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" icon="hamburger" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :fixed="fixed"
+      v-touch:swipe.top="dialogInit"
+      app
+    >
+      <span>&copy; nuxt</span>
+    </v-footer>
+    <notifications group="foo" />
+  </v-app>
+</template>
+
+<script>
+export default {
+  transition: 'edit',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Templates',
+          to: '/'
+        },
+        {
+          icon: 'mdi-archive',
+          title: 'Archive',
+          to: '/archived'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'SIMPLE CONTENT EDITOR'
+    }
+  },
+  methods: {
+    dialogInit () {
+    }
+  }
+}
+</script>
---
+++ pages/archived.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-layout
+    column
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <v-subheader>
+        Your archived templates
+      </v-subheader>
+
+      <v-list-item-group
+        v-model="selected"
+        active-class=""
+      >
+        <template v-for="(item, index) in items">
+          <v-list-item v-if="item.isArchived" :key="item.id">
+            <template>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title" />
+                <v-list-item-subtitle v-text="item.subtitle" />
+              </v-list-item-content>
+            </template>
+          </v-list-item>
+
+          <v-divider
+            v-if="index + 1 < items.length && item.isArchived"
+            :key="`d${index}`"
+          />
+        </template>
+      </v-list-item-group>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  components: {},
+  data: () => ({
+    selected: null,
+    items: [
+      {
+        id: 0,
+        isArchived: true,
+        title: 'title',
+        subtitle: 'Description of template'
+      },
+      {
+        id: 1,
+        isArchived: true,
+        title: 'title',
+        subtitle: 'Description of template'
+      },
+      {
+        id: 2,
+        isArchived: false,
+        title: 'title',
+        subtitle: 'Description of template'
+      },
+      {
+        id: 3,
+        isArchived: false,
+        title: 'title',
+        subtitle: 'Description of template'
+      },
+      {
+        id: 4,
+        isArchived: false,
+        title: 'title',
+        subtitle: 'Description of template'
+      }
+    ]
+  }),
+  computed: {
+    hasTemplates () {
+      return this.items.length > 0
+    }
+  },
+  methods: {
+    remove (index) {
+      this.items.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
